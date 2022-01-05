ce6cca02ce3f12891abcc0e28e20ef54b9f3c38d
Timothy Angus <timangus321@gmail.com>
2022-01-05 17:34:02 +0000

    commence work on tests

---
+++ src/components/nextArrow.test.js
@@ -0,0 +1,17 @@
+// Test not working 
+
+// import { render, screen, container } from "@testing-library/react";
+// import "@testing-library/jest-dom/extend-expect";
+// import NextArrow from "./next-arrow";
+// import App from 'App';
+
+
+// describe('Render Next Pokemon in Pokemon Card' , () => {
+//     test('On click the next button displays the next pokemon', () => {
+//         render(<App  />)
+
+//         const nextArrowElm = screen.getByTitle('.right-arrow')
+//         expect(nextArrowElm).toBeInTheDocument();
+
+//     })
+// })
