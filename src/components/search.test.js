7f59415d0d7d3a5f6ae7572fa9e2dc16d951c3ae
Timothy Angus <timangus321@gmail.com>
2022-01-10 14:50:08 +0000

    Test for search - need work mocking api calls

---
+++ src/components/search.test.js
@@ -0,0 +1,39 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Search from "./search";
+import PokemonCard from "./pokemon-card";
+import mockPokemonDetails from "mock-data/mockPokemonDetails";
+
+
+
+const mockPokemonSearch = jest.fn();
+
+const MockPokemonCard = () => {
+  return (
+    <PokemonCard
+      pokemonDetails={mockPokemonDetails}
+      pokeName={mockPokemonDetails.name}
+      pokeId={mockPokemonDetails.id}
+      pokeImg={mockPokemonDetails.sprites.front_default}
+    />
+  );
+};
+
+describe("Searching for a pokemon should return the pokemon of the same name", () => {
+  test("Searching a pokemon should return requested pokemon info", async () => {
+    render(<MockPokemonCard pokemonDetails={mockPokemonDetails} />);
+    render(<Search pokemonSearch={mockPokemonSearch} />);
+
+    const searchInput = screen.getByPlaceholderText("Search Pokemon");
+    expect(searchInput).toBeInTheDocument();
+    fireEvent.change(searchInput, { target: { value: "ditto"} })
+
+    const searchButton = screen.getByText("Search");
+    expect(searchButton).toBeInTheDocument();
+    fireEvent.click(searchButton);
+
+    const h3Elm = screen.getByText("ditto")
+    expect(h3Elm).toBeInTheDocument();
+
+  });
+});
