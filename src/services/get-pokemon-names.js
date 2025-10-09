f1076c074884835c4a457148731d106ccc5b585b
Timothy Angus <timangus321@gmail.com>
2025-10-09 16:48:56 +0100

    add proper total and add enter for triggering search

--- src/services/get-pokemon-names.js
+++ src/services/get-pokemon-names.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const pokeNamesUrl = 'https://pokeapi.co/api/v2/pokemon?limit=898';
+const pokeNamesUrl = 'https://pokeapi.co/api/v2/pokemon?limit=1025';
 
 export async function getPokemonNames() {
   const allPokemonNames = await axios.get(pokeNamesUrl).catch((error) => console.log(error));
