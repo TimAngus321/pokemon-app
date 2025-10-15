71bb9a7ef5868c467b07e0c8f19aa74e5ada2f30
Timothy Angus <timangus321@gmail.com>
2025-10-15 22:11:40 +0100

    add styling for game components that replace arrows

--- src/components/arrows/next-arrow.jsx
+++ src/components/arrows/next-arrow.jsx
@@ -1,29 +1,24 @@
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import './arrows.scss';
+import "./arrows.scss";
 
 const NextArrow = (props) => {
   console.log("props.whosThatPokemonMode: ", props);
   return (
-    <div>
-    {props.whosThatPokemonMode ? (
-      <div className="correct-answers">
-        <p>Correct guesses: </p>
-        <p>{props.correctAnswersCount}</p>
-      </div>
-    )
-  : (
-    <div
-      className="right-arrow-container"
-      onClick={(e) => props.nextPokemon()}
-      data-testid="next-arrow-test"
-    >
-      <FontAwesomeIcon
-        icon={faArrowRight}
-        className="right-arrow"
-      />
-    </div>
-    )}
+    <div className="right-arrow-container">
+      {props.whosThatPokemonMode ? (
+        <div className="correct-answers">
+          <p>
+            Correct
+            <br /> guesses:{" "}
+          </p>
+          <p>{props.correctAnswersCount}</p>
+        </div>
+      ) : (
+        <div onClick={(e) => props.nextPokemon()} data-testid="next-arrow-test">
+          <FontAwesomeIcon icon={faArrowRight} className="right-arrow" />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/arrows/previous-arrow.jsx
+++ src/components/arrows/previous-arrow.jsx
@@ -1,28 +1,20 @@
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import './arrows.scss';
-
+import "./arrows.scss";
 
 const PreviousArrow = (props) => {
   return (
-    <div>
-     {props.whosThatPokemonMode ? (
-      <div className="incorrect-answers">
-        <p>Incorrect answers: </p>
-        <p>{props.incorrectAnswersCount}</p>
-      </div>
-    )
-  : (
-    <div
-      className="left-arrow-container"
-      onClick={(e) => props.previousPokemon()}
-    >
-      <FontAwesomeIcon
-        className="left-arrow"
-        icon={faArrowLeft}
-      />
-    </div>
-    )}
+    <div className="left-arrow-container">
+      {props.whosThatPokemonMode ? (
+        <div className="incorrect-answers">
+          <p>Incorrect answers: </p>
+          <p>{props.incorrectAnswersCount}</p>
+        </div>
+      ) : (
+        <div onClick={(e) => props.previousPokemon()}>
+          <FontAwesomeIcon className="left-arrow" icon={faArrowLeft} />
+        </div>
+      )}
     </div>
   );
 };
